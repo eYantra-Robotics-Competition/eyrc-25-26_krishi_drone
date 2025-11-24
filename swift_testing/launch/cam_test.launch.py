f5e5cfb288d4ff57c02f503ba38a1d3aa20c6575
mahi-vea <shakthimagenderan@gmail.com>
2025-11-24 18:28:31 +0530

    stage2 hardware testing

---
+++ swift_testing/launch/cam_test.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+
+        Node(
+            package='usb_cam',
+            name='usb_cam',
+            executable='usb_cam_node_exe',
+            output='screen',
+            parameters=[{
+                'video_device': '/dev/video4',
+                'image_width': 1920,
+                'image_height': 1080,
+                'pixel_format': 'mjpeg2rgb',
+                'io_method': 'mmap',
+                'framerate': 30.0,
+                'camera_frame_id': 'usb_cam',
+                'av_device_format': 'YUV422P',
+            }]
+        ),
+
+        Node(
+            package='swift_testing',
+            executable='cam_test',
+            name='cam_test',
+            output='screen'
+        )
+    ])
